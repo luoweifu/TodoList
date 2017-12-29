0767ac2c62aae178bc7e6f57cc5c1a7260663dd9
Spencer.Luo <2853263095@qq.com>
2017-12-29 13:07:04 +0800

    update sql script.

--- db/database.sql
+++ db/database.sql
@@ -4,12 +4,12 @@ CREATE TABLE user(
   pk_id BIGINT PRIMARY KEY NOT NULL  AUTO_INCREMENT,
   gmt_create DATETIME NOT NULL,
   gmt_modified DATETIME NOT NULL,
-  name VARCHAR(20) NOT NULL,
+  uk_account VARCHAR(20) NOT NULL,
   nick_name VARCHAR(20),
-  phone_nou CHAR(11),
-  email VARCHAR(50)
+  uk_phone_num CHAR(11),
+  uk_email VARCHAR(50),
+  password VARCHAR(50)
 );
-
 
 # Category
 CREATE TABLE category(
@@ -43,3 +43,6 @@ CREATE TABLE task(
   uk_categroy BIGINT,
   uk_tag BIGINT
 );
+
+
+INSERT INTO user(gmt_create, gmt_modified, uk_account, nick_name, uk_phone_num, uk_email, password) VALUES (NOW(), NOW(), 'Spencer', 'Spencer', '18500315888', 'luoweifu@126.com', 'Spencer.Luo');
